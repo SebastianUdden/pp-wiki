c02033b5a185886b6b6f35c6586c4e30378797da
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-11-14 18:24:53 +0100

    urls

---
+++ src/constants/urls.js
@@ -0,0 +1,3 @@
+export const apiUrl = process.env.GATSBY_API_URL
+export const defaultImageUrl =
+  "https://cdn.iconscout.com/icon/premium/png-256-thumb/human-user-profile-man-face-avatar-51412.png"
